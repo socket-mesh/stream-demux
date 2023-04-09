1d038634f1367dffa1de8e83ca25ed3bfb577d15
Greg Kimmy <greg.kimmy@gmail.com>
2023-04-09 09:44:14 -0500

    Added stream demux wrapper class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { StreamDemux } from "./stream-demux";
 export { StreamDemuxStats } from "./stream-demux-stats";
+export { StreamDemuxWrapper } from "./stream-demux-wrapper";
 export { DemuxedConsumableStream } from "./demuxed-consumable-stream";
---
+++ src/stream-demux-wrapper.ts
@@ -0,0 +1,61 @@
+import { DemuxedConsumableStream } from "./demuxed-consumable-stream";
+import { StreamDemux } from "./stream-demux";
+import { StreamDemuxStats } from "./stream-demux-stats";
+
+export class StreamDemuxWrapper<T> {
+	private readonly _streamDemux: StreamDemux<T>
+
+	constructor(stream: StreamDemux<T>) {
+		this._streamDemux = stream;
+	}
+
+	stream(name: string): DemuxedConsumableStream<T> {
+		return this._streamDemux.stream(name);
+	}
+
+	close(): void;
+	close(name: string): void;
+	close(name?: string): void {
+		if (name === undefined) {
+			this._streamDemux.closeAll();
+		}
+
+		this._streamDemux.close(name);
+	}
+
+	kill(): void;
+	kill(consumerId: number): void;
+	kill(name: string): void;
+	kill(name?: string | number): void {
+		if (name === undefined) {
+			this._streamDemux.killAll();
+			return;
+		}
+		
+		this._streamDemux.kill(name as any);
+	}
+
+	getConsumerStats(): StreamDemuxStats[];
+	getConsumerStats(name: string): StreamDemuxStats[];
+	getConsumerStats(consumerId: number): StreamDemuxStats;
+	getConsumerStats(consumerId?: number | string): StreamDemuxStats[] | StreamDemuxStats {
+		return this._streamDemux.getConsumerStats(consumerId as any);
+	}
+	
+	getBackpressure(): number;
+	getBackpressure(name: string): number;
+	getBackpressure(consumerId: number): number;
+	getBackpressure(consumerId?: number | string): number {
+		return this._streamDemux.getBackpressure(consumerId as any);
+	}
+
+	hasConsumer(consumerId: number): boolean;
+	hasConsumer(name: string, consumerId: number): boolean;
+	hasConsumer(name: string | number, consumerId?: number): boolean {
+		if (typeof name === "string") {
+			return this._streamDemux.hasConsumer(name, consumerId);
+		}
+
+		return this._streamDemux.hasConsumer(name /* consumerId */);
+	}
+}
